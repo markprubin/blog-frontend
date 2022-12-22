86067cf372604028084a96a3f947879d4579b1e5
markprubin <mark.p.rubin@gmail.com>
2022-12-22 13:18:35 -0700

    adds main.jsx stuff

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,13 @@ import App from "./App";
 import "./index.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://legendary-souffle-646afd.netlify.app";
